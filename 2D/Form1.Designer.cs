94b8e2eb1acd92ce6b7cb3128332ce0d90c4af0d
AntonyChatt <nyanchatgalaxy1988@gmail.com>
2020-03-25 22:38:15 +0700

    yyyyyyy matrix

--- 2D/Form1.Designer.cs
+++ 2D/Form1.Designer.cs
@@ -29,11 +29,17 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Location = new System.Drawing.Point(3, 3);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -41,15 +47,75 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button3);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Location = new System.Drawing.Point(12, 7);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(84, 148);
+            this.panel1.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Location = new System.Drawing.Point(3, 32);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Turn";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Location = new System.Drawing.Point(3, 61);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Scale";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Enabled = false;
+            this.button4.Location = new System.Drawing.Point(3, 90);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 2;
+            this.button4.Text = "Reflection";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Enabled = false;
+            this.button5.Location = new System.Drawing.Point(3, 119);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 3;
+            this.button5.Text = "Shift";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.Button5_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.InactiveCaption;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "2D";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
@@ -57,6 +123,11 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- 2D/Form1.cs
+++ 2D/Form1.cs
@@ -12,37 +12,91 @@ namespace _2D
 {
     public partial class Form1 : Form
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
+        Pen myPen = new Pen(Color.MidnightBlue, 2);
+        Brush myBrush = new SolidBrush(Color.MidnightBlue);
+        Pen polygonPen = new Pen(Color.SlateBlue, 2);
+        Pen reflectPen = new Pen(Color.HotPink, 2);
+        Pen turnPen = new Pen(Color.Tomato, 2);
+        Pen scalePen = new Pen(Color.Plum, 2);
+        Pen shiftPen = new Pen(Color.DarkTurquoise, 2);
 
-        void drawAxises(Graphics g, Pen myPen, Brush myBrush)
+        float Fi = (float)(0.5 * 180 / Math.PI);
+
+        PointF[] global = new PointF[] { };
+        float[,] globalMatrix = new float[3, 6];
+        float sum = 0;
+        float[,] buff = new float[3, 6];
+
+        float x0;
+        float y0;
+        float interval;
+
+        float[,] reflection =  
         {
-            g.DrawLine(myPen, ClientSize.Width / 2, 0, ClientSize.Width / 2, ClientSize.Height);
-            g.DrawLine(myPen, 0, ClientSize.Height / 2, ClientSize.Width, ClientSize.Height / 2);
+            { 1, 0, 0 },
+            { 0, -1, 0 },
+            { 0, 0, 1 }
+        };
+
+        float[,] scale =
+        {
+             { -(float)0.5, 0, 0 },
+             { 0, -(float)0.5, 0 },
+             { 0, 0, 1 }
+        };
+
+        float[,] shift = 
+        {
+            {-1,0,0 },
+            {0,-1,0 },
+            {50,40,1 }
+        };
+
+        void multiplier(float[,] A)
+        {         
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    for (int k = 0; k < 3; k++)
+                    {
+                        sum += A[k, i] * globalMatrix[k, j];
+                    }
+
+                    buff[i, j] = sum;
+                    sum = 0;
+                }              
+            }            
+        }     
+
+        void drawAxises(Graphics g, float x0, float y0)
+        {
+            g.Transform = new System.Drawing.Drawing2D.Matrix(1, 0, 0, 1, x0, y0);
+            
+            g.DrawLine(myPen, -x0, 0, x0, 0);
+            g.DrawLine(myPen, 0, -y0, 0, y0);
 
             PointF[] Y = new PointF[]
             {
-                 new PointF(ClientSize.Width / 2, 0),
-                 new PointF((ClientSize.Width / 2) - 5, 5),
-                 new PointF((ClientSize.Width / 2) + 5, 5)
+                 new PointF(0, -y0),
+                 new PointF(5, -y0 + 5),
+                 new PointF(-5, -y0 + 5)
             };
 
             PointF[] X = new PointF[]
             {
-                 new PointF(ClientSize.Width, ClientSize.Height / 2),
-                 new PointF((ClientSize.Width) - 5,(ClientSize.Height / 2) - 5),
-                 new PointF((ClientSize.Width ) - 5,(ClientSize.Height / 2) + 5)
+                 new PointF(x0, 0),
+                 new PointF(x0 - 5, -5),
+                 new PointF(x0 - 5, 5)
             };
 
             string y = "y";
             string x = "x";
             string zero = "0";
 
-            PointF forY = new PointF((ClientSize.Width / 2) - 20, 0);
-            PointF forX = new PointF(ClientSize.Width - 15, (ClientSize.Height / 2) + 5);
-            PointF forZero = new PointF((ClientSize.Width / 2) - 15, (ClientSize.Height / 2) + 5);
+            PointF forY = new PointF(-20, -y0);
+            PointF forX = new PointF(x0 - 15, 5);
+            PointF forZero = new PointF(-15, 5);
 
             Font f = new Font(this.Font, FontStyle.Bold);
 
@@ -54,34 +108,180 @@ namespace _2D
             g.FillPolygon(myBrush, X);
         }
 
-        void drawPolygon(Graphics g, Pen polygonPen)
+        void drawPolygon(Graphics g)
         {
-            int interval = ClientSize.Width / 20;
+           float interval = ClientSize.Width / 20;
 
-            PointF[] polygon = new PointF[]
+            PointF[] global = 
             {
-                new PointF((ClientSize.Width / 2) - 2 * interval, (ClientSize.Height / 2) - 2 * interval),
-                new PointF((ClientSize.Width / 2) - 2 * interval, (ClientSize.Height / 2) + 2 * interval),
-                new PointF((ClientSize.Width / 2) + 2 * interval, (ClientSize.Height / 2) + 2 * interval),
-                new PointF((ClientSize.Width / 2) + 3 * interval, (ClientSize.Height / 2) + 3 * interval),
-                new PointF((ClientSize.Width / 2) + 4 * interval, (ClientSize.Height / 2)),
-                new PointF((ClientSize.Width / 2) + 2 * interval, (ClientSize.Height / 2) - 2 * interval)
+                new PointF(-2 * interval, -2 * interval),
+                new PointF(-2 * interval, 2 * interval),
+                new PointF(2 * interval, 2 * interval),
+                new PointF(3 * interval, 3 * interval),
+                new PointF(4 * interval, 0),
+                new PointF(2 * interval,-2 * interval)
             };
 
-            g.DrawPolygon(polygonPen, polygon);
+            for ( int i = 0; i < 3; i++ )
+            {
+                for ( int j = 0; j < 6; j++ )
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            globalMatrix[i, j] = -global[j].X;
+                            break;
+                        case 1:
+                            globalMatrix[i, j] = -global[j].Y;
+                            break;
+                        case 2:
+                            globalMatrix[i, j] = 1;
+                            break;
+                        default:
+                            break;
+                    }                    
+                }
+            }
+
+            g.DrawPolygon(polygonPen, global);
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        void reflectPolygon(Graphics g)
+        {
+
+            multiplier( reflection );
+
+            PointF[] global =
+            {
+                new PointF(buff[0,0], buff[1,0]),
+                new PointF(buff[0,1], buff[1,1]),
+                new PointF(buff[0,2], buff[1,2]),
+                new PointF(buff[0,3], buff[1,3]),
+                new PointF(buff[0,4], buff[1,4]),
+                new PointF(buff[0,5], buff[1,5]),
+            };
+
+            g.DrawPolygon(reflectPen, global);
+        }
+  
+        void turnPolygon(Graphics g) 
+        {
+            float[,] turn = {
+                { (float)(Math.Cos(Fi)), (float)(-Math.Sin(Fi)), 0 },
+                { (float)(Math.Sin(Fi)), (float)(Math.Cos(Fi)), 0 },
+                { 0, 0, 1 }
+            };
+
+            multiplier( turn );
+
+           PointF[] global =
+           {
+                new PointF(buff[0,0], buff[1,0]),
+                new PointF(buff[0,1], buff[1,1]),
+                new PointF(buff[0,2], buff[1,2]),
+                new PointF(buff[0,3], buff[1,3]),
+                new PointF(buff[0,4], buff[1,4]),
+                new PointF(buff[0,5], buff[1,5]),
+            };
+
+            g.DrawPolygon(turnPen, global);
+        } 
+
+        void scalePolygon(Graphics g) 
+        {
+            multiplier( scale );
+
+            PointF[] global =
+            {
+                new PointF(buff[0,0], buff[1,0]),
+                new PointF(buff[0,1], buff[1,1]),
+                new PointF(buff[0,2], buff[1,2]),
+                new PointF(buff[0,3], buff[1,3]),
+                new PointF(buff[0,4], buff[1,4]),
+                new PointF(buff[0,5], buff[1,5]),
+            };
+
+            g.DrawPolygon(scalePen, global);
+        }
+
+        void shiftPolygon(Graphics g)
+        {
+            multiplier( shift );
+
+            PointF[] global =
+            {
+                new PointF(buff[0,0], buff[1,0]),
+                new PointF(buff[0,1], buff[1,1]),
+                new PointF(buff[0,2], buff[1,2]),
+                new PointF(buff[0,3], buff[1,3]),
+                new PointF(buff[0,4], buff[1,4]),
+                new PointF(buff[0,5], buff[1,5]),
+            };
+
+            g.DrawPolygon(shiftPen, global);
+        }        
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)  // Draw axises and polygon
+
         {
             Refresh();
             Graphics g = this.CreateGraphics();
-            Pen myPen = new Pen(Color.MidnightBlue, 2);  
-            Brush myBrush = new SolidBrush(Color.MidnightBlue); 
-            Pen polygonPen = new Pen(Color.SlateBlue, 2);  
-            drawAxises(g, myPen, myBrush);
-            drawPolygon(g, polygonPen);
+
+            x0 = ClientSize.Width / 2;
+            y0 = ClientSize.Height / 2;
+
+            interval = ClientSize.Width / 20;
+
+            drawAxises(g, x0, y0);
+            drawPolygon(g);
+
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;          
         }
 
+         private void Button2_Click(object sender, EventArgs e) // turn it for 0.5 radians
+         {
+            Graphics g = this.CreateGraphics();
 
-    }
+            drawAxises(g, x0, y0);
+            turnPolygon(g);
+         }
+        
+         private void Button3_Click(object sender, EventArgs e) //scale for 1/2
+         {
+            Graphics g = this.CreateGraphics();
+
+            drawAxises(g, x0, y0);
+            scalePolygon(g);
+        }
+    
+        private void Button4_Click(object sender, EventArgs e) // reflect polygon
+        {
+            Graphics g = this.CreateGraphics();
+            
+            drawAxises( g,x0, y0);
+            reflectPolygon(g);
+        }
+
+        private void Button5_Click(object sender, EventArgs e) // Shift polygon for 50 & 40 px
+        {
+            Graphics g = this.CreateGraphics();
+
+            drawAxises(g, x0, y0);
+            shiftPolygon(g);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
 }
+    }
+   
